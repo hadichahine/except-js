089a83ea64a8a9f9ea354d9cc11a9ae42532dc9c
Hadi Chahine <hadi.n.chahine@gmail.com>
2020-07-28 03:18:23 +0300

    Remove confusing test matcher and place logic into test.

--- tests/throwexception.test.js
+++ tests/throwexception.test.js
@@ -1,21 +1,14 @@
 const { create, handle } = require('../src/except')
 
-beforeAll(() => {
-    expect.extend({
-        toThrowCustomException(recievedException, exceptionClass){
-            let run
-            handle(recievedException, exceptionClass, () => {
-                run = true
-            })
-            return run ? {pass: true, message: 'handled successfully.'} :
-                         {pass: false, message: 'not handled successfully.'} 
-        }
-    })
-});
-
 test('test that exception is handled correctly.', () => {
     const TestException = create('TestException')
-    expect(() => {
+    let isHandlerRun
+    try {
         throw new TestException()
-    }).toThrowCustomException(TestException)
+    }catch(exception){
+        handle(exception, TestException, () => {
+            isHandlerRun = true
+        })
+    }
+    expect(isHandlerRun).toBe(true)
 })
