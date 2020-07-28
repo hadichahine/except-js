7275a6e59d94bfc8d5a1cd4dc57ab89773d733d0
Hadi Chahine <hadi.n.chahine@gmail.com>
2020-07-28 03:19:12 +0300

    Clarify test conditions by testname.

--- tests/throwexception.test.js
+++ tests/throwexception.test.js
@@ -1,6 +1,6 @@
 const { create, handle } = require('../src/except')
 
-test('test that exception is handled correctly.', () => {
+test('test that exception is handled correctly when 1 handle is used.', () => {
     const TestException = create('TestException')
     let isHandlerRun
     try {
